c9f39fb15407fbb93e2aa6dbdcbb5a41e073217f
JonatanMendez349 <jonatanmendezpujols@gmail.com>
2026-08-04 13:27:53 -0400

    Recompila el build web desde cero (flutter clean) para registrar bien image_picker
    El build anterior no incluia la implementacion web de image_picker
    (cache de compilacion desactualizado), lo que causaba
    'No implementation found for method pickImage on channel
    plugins.flutter.io/image_picker' al intentar abrir la camara.

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "07a285d86532a9ed90827cbce4e43ec4
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3aac2e940b9082fafb4478d052e4885a",
+"flutter_bootstrap.js": "6974e5580ab34dd9a9eb2fe24d3098e9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "da864a418f88f38be06d51607f8eae04",
 "/": "da864a418f88f38be06d51607f8eae04",
-"main.dart.js": "9eba31b566332d154e9250f63038a3d8",
+"main.dart.js": "2bf78d58c708c5fddbd1b3309e3c1e18",
 "manifest.json": "8c6347bd59a00c3fc736af3b6564fdc0",
 "version.json": "6c4418555881bfaba58c38af3f3e74b9"};
 // The application shell files that are downloaded before a service worker can
